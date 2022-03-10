9a854888112b4c82af7d15b1d762f8e48cd32947
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-11 00:28:12 +0530

    Completed the task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Markdown from 'marked-react';
 
 export default function App() {
+  const [markdown, setMarkDown] = useState('# sup');
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea onChange={e => setMarkDown(e.target.value)} value={markdown}/>
+      <div className="preview">
+        <Markdown>{markdown}</Markdown>
+      </div>
     </div>
   );
 }
